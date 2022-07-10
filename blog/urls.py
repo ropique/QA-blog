a3db08272271a3cb39f542f4227272410dd78ec9
ropique <anush_av@instigatemobile.com>
2022-07-10 04:15:25 +0400

    path contacts

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,7 @@ from .views import  PostDetailView,  PostListView, PostCreateView
 urlpatterns = [
     
     path('', PostListView.as_view(), name='blog-home'),
-    path('about/', views.about, name = 'blog-about'),
+    path('contacts/', views.about, name = 'blog-about'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/add_post/', PostCreateView.as_view(), name='add_post'),
     path('search_res/', views.search_res, name ='search_res'),
